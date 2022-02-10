4dee45dbd2ab385dede612d1f7f9e844276b7b0d
om jha <jhaom1521@gmail.com>
2022-02-10 21:28:43 +0530

    basic info

--- baisic-js/js-abcd/assets/scripts/app.js
+++ baisic-js/js-abcd/assets/scripts/app.js
@@ -1,14 +1,52 @@
 // Declare variable and Constants before using it
 //Methods can be placed anywhere
-
+/*
+Data Types :
+Numbers
+String
+Boolean
+*/
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
+function createWriteLog(operator, resultBeforeCalculation, calcNumber) {
+  const calcDescription = `${resultBeforeCalculation} ${operator}  ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
 function add() {
-  currentResult = currentResult + parseInt(userInput.value);
-  outputResult(currentResult, "");
+  const enteredNum = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNum;
+  createWriteLog("+", initialResult, enteredNum);
+}
+
+function substract() {
+  const enteredNum = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNum;
+  createWriteLog("-", initialResult, enteredNum);
+}
+
+function multiply() {
+  const enteredNum = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNum;
+  createWriteLog("*", initialResult, enteredNum);
+}
+
+function divide() {
+  const enteredNum = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNum;
+  createWriteLog("/", initialResult, enteredNum);
 }
 
 addBtn.addEventListener("click", add);
-
-outputResult(currentResult, calculationDescription);
+subtractBtn.addEventListener("click", substract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
