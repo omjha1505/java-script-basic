3e84a5f79f1be16ea2d3eb0e2c78c08a3d585dc1
om jha <jhaom1521@gmail.com>
2022-02-10 21:59:53 +0530

    basic info

--- baisic-js/js-abcd/assets/scripts/app.js
+++ baisic-js/js-abcd/assets/scripts/app.js
@@ -5,9 +5,17 @@ Data Types :
 Numbers
 String
 Boolean
+Object
+Array
+null , undefined , NaN
+undefined : default values of uninitalized variables
+null      : we can use this to reset/clear something
+NaN       : something like 12/'a'
+typeof keyword
 */
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 function getUserNumberInput() {
   return parseInt(userInput.value);
@@ -18,11 +26,28 @@ function createWriteLog(operator, resultBeforeCalculation, calcNumber) {
   outputResult(currentResult, calcDescription);
 }
 
+function writeToLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
 function add() {
   const enteredNum = getUserNumberInput();
   const initialResult = currentResult;
   currentResult = currentResult + enteredNum;
   createWriteLog("+", initialResult, enteredNum);
+  writeToLog("ADD", initialResult, enteredNum, currentResult);
 }
 
 function substract() {
@@ -30,6 +55,7 @@ function substract() {
   const initialResult = currentResult;
   currentResult = currentResult - enteredNum;
   createWriteLog("-", initialResult, enteredNum);
+  writeToLog("SUBSTRACT", initialResult, enteredNum, currentResult);
 }
 
 function multiply() {
@@ -37,6 +63,7 @@ function multiply() {
   const initialResult = currentResult;
   currentResult = currentResult * enteredNum;
   createWriteLog("*", initialResult, enteredNum);
+  writeToLog("MULTIPLY", initialResult, enteredNum, currentResult);
 }
 
 function divide() {
@@ -44,6 +71,7 @@ function divide() {
   const initialResult = currentResult;
   currentResult = currentResult / enteredNum;
   createWriteLog("/", initialResult, enteredNum);
+  writeToLog("DIVIDE", initialResult, enteredNum, currentResult);
 }
 
 addBtn.addEventListener("click", add);
